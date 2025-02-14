2056d3a02f5c8bb8d1b7d6acbabff3d295addef6
Igor Khitryk <igor.khitryk@wolt.com>
2025-02-15 00:00:46 +0100

    Add http.request to requested resource

--- types.go
+++ types.go
@@ -1,24 +1,19 @@
 package connexions_plugin
 
 import (
-	"net/http"
-	"net/url"
+    "net/http"
 )
 
 // RequestedResource represents the current request that is being processed.
 // Resource is the openapi resource path, i.e. /pets, /pets/{id}
-// Method is the HTTP method, i.e. GET, POST, PUT, DELETE
-// URL is the full URL of the request
-// Headers is the request headers
 // Body is the request body if method is not GET
 // Response is the current response if present
+// Request is the current http request
 type RequestedResource struct {
-	Resource string
-	Method   string
-	URL      *url.URL
-	Headers  map[string][]string
-	Body     []byte
-	Response *HistoryResponse
+    Resource string
+    Body     []byte
+    Response *HistoryResponse
+    Request  *http.Request
 }
 
 // HistoryResponse represents the response that was generated or received from the server.
@@ -26,11 +21,7 @@ type RequestedResource struct {
 // StatusCode is the HTTP status code returned
 // IsFromUpstream is true if the response was received from the upstream server
 type HistoryResponse struct {
-	Data           []byte
-	StatusCode     int
-	IsFromUpstream bool
+    Data           []byte
+    StatusCode     int
+    IsFromUpstream bool
 }
-
-// RequestTransformer is a function that modifies the request before it is sent to the server.
-// Resource represents openapi resource path, i.e. /pets, /pets/{id}
-type RequestTransformer func(resource string, request *http.Request) (*http.Request, error)
