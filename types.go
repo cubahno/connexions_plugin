6ae0c1fc1a13c320aad3241dcb98c74e161c31b4
Igor Khitryk <igor.khitryk@wolt.com>
2025-02-19 22:14:27 +0100

    Add service storage interface

--- types.go
+++ types.go
@@ -9,11 +9,19 @@ import (
 // Body is the request body if method is not GET
 // Response is the current response if present
 // Request is the current http request
+// ServiceStorage is the thread-safe storage that is used to store and retrieve data for this service.
+// Auto-cleared by history storage timeout.
 type RequestedResource struct {
-    Resource string
-    Body     []byte
-    Response *HistoryResponse
-    Request  *http.Request
+    Resource       string
+    Body           []byte
+    Response       *HistoryResponse
+    Request        *http.Request
+    ServiceStorage MemoryStorage
+}
+
+type MemoryStorage interface {
+    Get(key string) any
+    Set(key string, value any)
 }
 
 // HistoryResponse represents the response that was generated or received from the server.
